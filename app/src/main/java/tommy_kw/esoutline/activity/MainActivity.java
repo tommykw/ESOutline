c6199705dfd4ab58e31ab16c2bf5f70e0215b32a
kenji tomita <tomita@diverse-inc.com>
2015-06-25 14:43:37 +0900

    added TabLayout for MainActivity

--- app/src/main/java/tommy_kw/esoutline/activity/MainActivity.java
+++ app/src/main/java/tommy_kw/esoutline/activity/MainActivity.java
@@ -1,37 +1,62 @@
 package tommy_kw.esoutline.activity;
 
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import tommy_kw.esoutline.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    @InjectView(R.id.tool_bar)
+    Toolbar mToolbar;
+    @InjectView(R.id.tab_layout)
+    TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        setSupportActionBar(mToolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+        initTabLayout();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initTabLayout() {
+        mTabLayout.setBackgroundColor(getResources().getColor(R.color.bg_brand));
+        mTabLayout.addTab(mTabLayout.newTab().setText("xxxx01"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("xxxx02"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("xxxx03"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("xxxx04"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("xxxx05"));
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == android.R.id.home) {
+            finish();
             return true;
         }
 
